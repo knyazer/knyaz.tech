511476d554d5bf0374d8ba0b97b4d9c14734b76b
Knyaz <knyaz.roma@gmail.com>
2021-11-20 19:38:53 +0300

    Initial commit

---
+++ src/main/java/tech/knyaz/server/api/APIController.java
@@ -0,0 +1,16 @@
+package tech.knyaz.server.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class APIController {
+
+	@GetMapping("/status")
+	public String status() {
+		return "Active";
+	}
+
+}
